242632b097e2bb97c41f9535fda52bc7d06f8781
Bart Massey <bart@po8.org>
2010-02-07 21:53:01 -0800

    added functions through databaseFindMessage

--- Notmuch.hs
+++ Notmuch.hs
@@ -5,9 +5,14 @@ import NOTMUCH_H
 
 import Control.Monad
 
--- XXX deriving Enum will only work if these fields are in
+requiredDatabaseVersion :: Int
+requiredDatabaseVersion = 1
+
+-- XXX Deriving Enum will only work if these fields are in
 -- the same order as in notmuch.h and there are no gaps
--- there
+-- there.
+-- XXX This should probably be thrown away as hidden
+-- internally.
 data Status = 
   StatusSuccess |
   StatusOutOfMemory |
@@ -36,9 +41,9 @@ databaseCreate name = do
        fail "database create failed"
   return db
 
--- XXX deriving Enum will only work if these fields are in
+-- XXX Deriving Enum will only work if these fields are in
 -- the same order as in notmuch.h and there are no gaps
--- there
+-- there.
 data DatabaseMode = 
     DatabaseModeReadOnly |
     DatabaseModeReadWrite
@@ -72,3 +77,70 @@ databaseNeedsUpgrade db = do
   case ug of
     0 -> return False
     _ -> return True
+
+
+statusCheck :: CInt -> IO ()
+statusCheck 0 = return ()
+statusCheck s = fail $ statusToString $ toEnum $ fromIntegral s
+
+type UpgradeCallback = String -> Double -> IO ()
+
+databaseUpgrade :: Database -> Maybe UpgradeCallback -> IO ()
+databaseUpgrade db (Just callback) = do
+  let ccb msg progress = do
+        cmsg <- peekCString msg
+        let cprogress = realToFrac progress
+        callback cmsg cprogress
+  cb <- w_notmuch_database_upgrade_1 ccb
+  s <- f_notmuch_database_upgrade db cb nullPtr
+  statusCheck s
+databaseUpgrade db Nothing = do
+  s <- f_notmuch_database_upgrade db nullFunPtr nullPtr
+  statusCheck s
+
+type Directory = Ptr S__notmuch_directory
+
+databaseGetDirectory :: Database -> String -> IO Directory
+databaseGetDirectory db path =
+    withCString path $ f_notmuch_database_get_directory db
+  
+type Message = ForeignPtr S__notmuch_message
+
+-- XXX We provide no way to request a null message pointer,
+-- so the message is always returned.  The finalizer will
+-- then eventually kill it if it is not needed.
+
+-- XXX This function will fail on dup adds, rather than
+-- succeed.  I have no idea what it should do, and this
+-- was easiest.
+databaseAddMessage :: Database -> String -> IO Message
+databaseAddMessage db filename = alloca msgFun where
+    msgFun msgPtr = do
+      let addMessage fn =
+              f_notmuch_database_add_message db fn msgPtr
+      s <- withCString filename addMessage
+      statusCheck s
+      cmsg <- peek msgPtr
+      newForeignPtr pf_notmuch_message_destroy cmsg
+
+-- XXX This function will fail on dup remove, rather than
+-- succeed.  I have no idea what it should do, and this
+-- was easiest.
+databaseRemoveMessage :: Database -> String -> IO ()
+databaseRemoveMessage db filename = do
+  let removeMessage fn = f_notmuch_database_remove_message db fn
+  s <- withCString filename removeMessage
+  statusCheck s
+
+-- XXX This might want to return a Maybe Message instead
+-- of failing if the message is not found.  I don't quite
+-- understand the use case yet.
+databaseFindMessage :: Database -> String -> IO Message
+databaseFindMessage db msgid = do
+  let findMessage mid =
+          f_notmuch_database_find_message db mid
+  cmsg <- withCString msgid findMessage
+  when (cmsg == nullPtr) $
+       fail "database find message failed"
+  newForeignPtr pf_notmuch_message_destroy cmsg
+  
