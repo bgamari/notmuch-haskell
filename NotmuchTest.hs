898fa9ba298a44b7dbb2dfdfc0b4e0399f670062
Bart Massey <bart@po8.org>
2010-02-12 02:23:52 -0800

    fleshed out test to full case

--- NotmuchTest.hs
+++ NotmuchTest.hs
@@ -1,14 +1,19 @@
 import Control.Monad
+import Data.Time
 import IO
 import Notmuch
 import System.Environment
     
 main = do
   argv <- getArgs
-  when (length argv > 0) $ do
-         db <- databaseCreate "/tmp"
-         databaseClose db
-  db <- databaseOpen "/tmp" DatabaseModeReadOnly
+  db <- if (length argv == 0)
+        then
+          (do
+            db <- databaseCreate "/tmp"
+            databaseClose db
+            databaseOpen "/tmp" DatabaseModeReadOnly)
+        else
+          databaseOpen (head argv) DatabaseModeReadOnly
   dbPath <- databaseGetPath db
   putStrLn $ "database is at " ++ dbPath
   version <- databaseGetVersion db
@@ -16,5 +21,24 @@ main = do
   upgrade <- databaseNeedsUpgrade db
   when upgrade $
        putStrLn "database needs upgrade"
+  query <- queryCreate db "subject:notmuch"
+  nquery <- queryCountMessages query
+  putStrLn $ "subject:notmuch returns " ++ show nquery ++ " results..."
+  threads <- queryThreads query
+  putStr $ show (length threads) ++ ": "
+  nthreadss <- mapM threadCountMessages threads
+  print nthreadss
+  let thread = head threads
+  subject <- threadGetSubject thread
+  putStrLn subject
+  messages <- threadGetToplevelMessages thread
+  let message = head messages
+  subject' <- messageGetHeader message "Subject"
+  putStrLn subject'
+  date <- messageGetDate message
+  print date
+  tz <- getTimeZone date
+  let localdate = utcToLocalTime tz date
+  print localdate
   databaseClose db
   return ()
